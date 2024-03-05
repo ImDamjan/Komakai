018396bd4e814dade5ffd49516256bb950ed888f
Aleksandra Stanic <astanic2001@gmail.com>
2024-03-05 19:46:02 +0100

    #13-Dodate komponente za lgoin i registraciju

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/auth', pathMatch: 'full' },
-  { path: 'auth' ,component: AuthComponent}
+  { path: 'auth' ,component: AuthComponent},
+  
+  { path: 'auth/login', component: LoginComponent },
+  { path: 'auth/register', component: RegisterComponent }
 ];
 
 @NgModule({
