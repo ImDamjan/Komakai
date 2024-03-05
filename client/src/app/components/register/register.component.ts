963cd03f701188b3ce11437a4d5e3f754c99e64d
Anastasija Ilicic <45-2021@pmf.kg.ac.rs>
2024-03-05 23:58:54 +0100

    #12 Dodana validacija i obrada forme za registraciju

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { RegisterService } from '../../services/register.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
+  registerForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private reg: RegisterService){}
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      name: ['',Validators.required],
+      surname: ['',Validators.required],
+      email: ['',Validators.required],
+      username: ['',Validators.required],
+      password: ['',Validators.required],
+      confirmPassword: ['',Validators.required]
+    })
+  }
+
+  onSubmit() {
+    if(this.registerForm.valid){
+      console.log(this.registerForm.value)
+      //send the obj to database
+      this.reg.register(this.registerForm.value)
+      .subscribe({
+        next:(res)=>{
+          alert(res.message)
+        },
+        error:(err)=>{
+          alert(err?.error.message)
+        }
+      })
+    }else{
+      //throw error
+      this.validateAllFormFields(this.registerForm);
+      alert("Your form is invalid")
+    }
+  }
+
+  private validateAllFormFields(formGroup: FormGroup){
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if(control instanceof FormControl){
+        control.markAsDirty({onlySelf:true});
+      }else if(control instanceof FormGroup){
+        this.validateAllFormFields(control)
+      }
+    })
+  }
 }
+
