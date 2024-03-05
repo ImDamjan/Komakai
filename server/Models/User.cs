26fc3e9a23ad4b7a4ddb16f4796b981da6fe12be
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-03-05 19:06:24 +0100

    Task #10.1 - Kreiran pocetni model korisnika

---
+++ server/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace server.Models
+{
+    public class User
+    {
+        public string Username { get; set; }= string.Empty;
+        public string Lastname { get; set; }= string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; }=string.Empty;
+    }
+}
