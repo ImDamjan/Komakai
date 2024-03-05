0ba9f15c7db8632384e146a5ac7a5beddbfa7f17
Aleksandra Stanic <astanic2001@gmail.com>
2024-03-05 11:56:22 +0100

    #13-izgled strane autorizacije

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
