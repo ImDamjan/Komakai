780a7ba43c71cfdc77152d3df2433e0aec83eef7
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-03-05 19:34:28 +0100

    #10.2 Dodata POST metoda za registraciju korisnika. Implementirano hashiranje lozinke.

---
+++ server/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using server.Models;
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user =new User();
+
+        //Registracija
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserRequestDto request)
+        {
+            //hash
+            string passwordHash
+                = BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+            user.Username=request.Username;
+            user.Lastname=request.Lastname;
+            user.PasswordHash=passwordHash;
+            user.Email=request.Email;
+
+            return Ok(user);
+        }
+    }
+
+}
