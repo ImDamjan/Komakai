af88da450ac0d37ff3410327fad79fa4e95820f8
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-03-05 19:52:07 +0100

    #10.3 Dodata POST metoda za login korisnika. Implementirana validacija podataka.

--- server/Controllers/AuthController.cs
+++ server/Controllers/AuthController.cs
@@ -8,7 +8,7 @@ namespace server.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        public static User user =new User();
+        public static User user = new User();
 
         //Registracija
         [HttpPost("register")]
@@ -18,13 +18,31 @@ namespace server.Controllers
             string passwordHash
                 = BCrypt.Net.BCrypt.HashPassword(request.Password);
 
-            user.Username=request.Username;
-            user.Lastname=request.Lastname;
-            user.PasswordHash=passwordHash;
-            user.Email=request.Email;
+            user.Username = request.Username;
+            user.Lastname = request.Lastname;
+            user.PasswordHash = passwordHash;
+            user.Email = request.Email;
 
             return Ok(user);
         }
+
+        //Login
+        [HttpPost("login")]
+        public ActionResult<User> Login(UserRequestDto request)
+        {
+            if(user.Username != request.Username)
+            {
+                return BadRequest("User not found");
+            }
+
+            if(!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+            {
+                return BadRequest("Wrong password");
+            }
+
+            return Ok(user);
+        }
+
     }
 
 }
