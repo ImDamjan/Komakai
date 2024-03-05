f17ed78f4a53a6208e161ef8a9c9d669dca2a371
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-03-05 19:12:43 +0100

    #10.1 Dodat DTO (Data transfer object)

---
+++ server/Models/UserRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace server.Models
+{
+    public class UserRequestDto
+    {
+        public required string Username { get; set; }
+        public required string Lastname { get; set; }
+        public required string Password { get; set; }
+        public required string Email { get; set; }
+    }
+}
