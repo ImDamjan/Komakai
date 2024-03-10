35ef0cf6cf83d4ae8fe69bdf0e8fa14e4e77ee23
Aleksandra Stanic <astanic2001@gmail.com>
2024-03-10 23:59:38 +0100

    #17-Prikaz navigation bar-a

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { AuthComponent } from './pages/auth/auth.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ReglogComponent } from './components/reglog/reglog.component';
+import { BodyComponent } from './pages/body/body.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/auth', pathMatch: 'full' },
-  { path: 'auth' ,component: AuthComponent}
+  { path: 'auth' ,component: AuthComponent},
+  {path: 'app', component: BodyComponent}
 ];
 
 @NgModule({
