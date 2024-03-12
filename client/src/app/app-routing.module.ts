c08132e2b488f7f4a635292579ba64f507cdef0e
Aleksandra Stanic <astanic2001@gmail.com>
2024-03-12 02:48:48 +0100

    Sredjen raspored fajlova, pages su cele strane, komponente su samo u komponentama

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth.component';
 import { LoginComponent } from './components/login/login.component';
-import { DashboardComponent } from './components/navigationbar/dashboard/dashboard.component';
-import { ProjectsComponent } from './components/navigationbar/projects/projects.component';
-import { ActivityComponent } from './components/navigationbar/activity/activity.component';
-import { TasksComponent } from './components/navigationbar/tasks/tasks.component';
-import { TeamsComponent } from './components/navigationbar/teams/teams.component';
-import { HelpcentreComponent } from './components/navigationbar/helpcentre/helpcentre.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ActivityComponent } from './pages/activity/activity.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
+import { TeamsComponent } from './pages/teams/teams.component';
+import { HelpcentreComponent } from './pages/helpcentre/helpcentre.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/auth', pathMatch: 'full' },
