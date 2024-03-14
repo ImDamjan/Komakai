c9d1acbeb0a7227be9db35f865c3a2533daff477
aleksa.kosovac <49-2021@pmf.kg.ac.rs>
2024-03-14 16:11:08 +0100

    #27 Pozivanje post metode potpuno premesteno u login.service

--- client/src/app/services/login.service.service.ts
+++ client/src/app/services/login.service.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../enviroments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,11 @@ export class LoginServiceService {
   private baseUrl:string = "${this.apiUrl}/Auth/";
   constructor(private http : HttpClient) { }
 
-  login(){
-    
+  login(userObj:any): Observable<any>{
+    return this.http.post('https://localhost:7152/api/Auth/login', userObj).pipe(catchError(this.handleError));
   }
 
+  handleError(error: HttpErrorResponse) {
+    return throwError(error);
+  }
 }
