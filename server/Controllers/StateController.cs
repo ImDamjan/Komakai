684a41a5061ab57c2bef6a75001c768e58e509a1
Stefan Vucicevic <stefanvucicevic002@gmail.com>
2024-03-15 22:10:16 +0100

    #44 Ubaceno uzmanje svih stateova/statusa

---
+++ server/Controllers/StateController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using server.Interfaces;
+using server.Mappers;
+
+namespace server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StateController : ControllerBase
+    {
+        private readonly IStateRepository _state_repo;
+        public StateController(IStateRepository state_repo)
+        {
+            _state_repo = state_repo;
+        }
+        [HttpGet("getAll")]
+        public async Task<IActionResult> GetAllRoles()
+        {
+            var states = await _state_repo.GetAllStatesAsync();
+            
+            var dtos = states.Select(s=>s.toStateDto());
+
+            return Ok(dtos);
+        }
+    }
+}
