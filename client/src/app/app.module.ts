10d89d6d7472d3735e8f8c16ef76b53942452a97
aleksa.kosovac <49-2021@pmf.kg.ac.rs>
2024-03-16 17:15:44 +0100

    #37 Kreirana direktiva za tooltip

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { ProjectPreviewComponent } from './components/project-preview/project-pr
 import { ProjectTaskComponent } from './components/project-task/project-task.component';
 import { AuthComponent } from './pages/auth/auth.component';
 import { ProjectStatusComponent } from './components/project-status/project-status.component';
+import { MyTooltipDirective } from './directives/my-tooltip.directive';
 
 @NgModule({
   declarations: [
@@ -39,7 +40,8 @@ import { ProjectStatusComponent } from './components/project-status/project-stat
     HelpcentreComponent,
     ProjectPreviewComponent,
     ProjectTaskComponent,
-    ProjectStatusComponent
+    ProjectStatusComponent,
+    MyTooltipDirective
   ],
   imports: [
     
---
+++ client/src/app/directives/my-tooltip.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appMyTooltip]'
+})
+export class MyTooltipDirective {
+
+  constructor() { }
+
+}
