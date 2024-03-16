20cdb99066b876ff97f76195c48078b53d584f61
aleksa.kosovac <49-2021@pmf.kg.ac.rs>
2024-03-16 19:58:17 +0100

    #37 Tooltip napravljen da se pojavljuje relativno od pozicije misa

--- client/src/app/directives/my-tooltip.directive.ts
+++ client/src/app/directives/my-tooltip.directive.ts
@@ -4,32 +4,44 @@ import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/
   selector: '[appMyTooltip]'
 })
 export class MyTooltipDirective {
+  private tooltip: HTMLElement | null = null;
 
-  constructor(private elRef: ElementRef,private renderer: Renderer2) { }
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   @Input('appTooltip') tooltipContent: string = '';
 
-  createToolTip(): HTMLElement{
+  createToolTip(): HTMLElement {
     const tooltip = this.renderer.createElement('div');
     const text = this.renderer.createText(this.tooltipContent);
-    this.renderer.appendChild(tooltip,text);
-    this.renderer.addClass(tooltip,'toolTipMy');
-    this.renderer.setStyle(tooltip,'position','absolute');
+    this.renderer.appendChild(tooltip, text);
+    this.renderer.addClass(tooltip, 'toolTipMy');
+    this.renderer.setStyle(tooltip, 'position', 'absolute');
     return tooltip;
   }
 
   @HostListener('mouseenter')
-  onMouseEnter(){
-    setTimeout(() => {
-      const myToolTip = this.createToolTip();
-      this.renderer.appendChild(this.elRef.nativeElement,myToolTip);
-    }, 300);
-  }
-  @HostListener('mouseout')
-  onMouseOut(){
-    const tooltip = this.elRef.nativeElement.querySelector('.toolTipMy')
-    this.renderer.removeChild(this.elRef.nativeElement, 'toolTipMy');
-    console.log('Mouse out')
+  onMouseEnter() {
+    if (!this.tooltip) {
+      this.tooltip = this.createToolTip();
+      this.renderer.appendChild(this.elRef.nativeElement, this.tooltip);
+    }
   }
 
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    if (this.tooltip) {
+      this.renderer.removeChild(this.elRef.nativeElement, this.tooltip);
+      this.tooltip = null; // Reset tooltip reference
+    }
+  }
+
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    if (this.tooltip) {
+      const x = event.clientX + 10; // Adjust the offset as needed
+      const y = event.clientY - this.tooltip.offsetHeight - 10; // Adjust the offset as needed
+      this.renderer.setStyle(this.tooltip, 'left', `${x}px`);
+      this.renderer.setStyle(this.tooltip, 'top', `${y}px`);
+    }
+  }
 }
