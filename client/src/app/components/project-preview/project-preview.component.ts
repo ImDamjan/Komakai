1e822027b21ba27efc2a48d248b89bc16552e90f
Anastasija Ilicic <45-2021@pmf.kg.ac.rs>
2024-03-16 19:32:17 +0100

    #38 Stavljen limit karaktera na naslov

--- client/src/app/components/project-preview/project-preview.component.ts
+++ client/src/app/components/project-preview/project-preview.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-project-preview',
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrl: './project-preview.component.css'
 })
 export class ProjectPreviewComponent {
+  title: string = 'Addodle';
+  truncatedTitle: string = '';
+  characterLimit: number = 0;
 
+  constructor() {
+    this.calculateCharacterLimit();
+    this.truncateTitle();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.calculateCharacterLimit();
+    this.truncateTitle();
+  }
+
+  calculateCharacterLimit() {
+    const screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      this.characterLimit = 5;
+    } else if (screenWidth > 768 && screenWidth < 1020) {
+      this.characterLimit = 10;
+    }
+    else {
+      this.characterLimit = 20;
+    }
+  }
+
+  truncateTitle() {
+    if (this.title.length > this.characterLimit) {
+      this.truncatedTitle = this.title.substring(0, this.characterLimit) + '...';
+    } else {
+      this.truncatedTitle = this.title;
+    }
+  }
 }
