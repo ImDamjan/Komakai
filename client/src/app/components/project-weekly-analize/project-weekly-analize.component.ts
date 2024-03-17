562f21830897d47c2731d22d8796de2d6274d3b6
Aleksandra Stanic <astanic2001@gmail.com>
2024-03-17 20:43:44 +0100

    #46-Kreiran line chart

--- client/src/app/components/project-weekly-analize/project-weekly-analize.component.ts
+++ client/src/app/components/project-weekly-analize/project-weekly-analize.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import Chart from 'chart.js/auto';
 
 @Component({
@@ -6,4 +6,32 @@ import Chart from 'chart.js/auto';
   templateUrl: './project-weekly-analize.component.html',
   styleUrl: './project-weekly-analize.component.css'
 })
-export class ProjectWeeklyAnalizeComponent{}
+export class ProjectWeeklyAnalizeComponent implements OnInit{
+  constructor() { }
+ ngOnInit(): void {
+    this.createChart();
+  }
+  public chart: any;
+
+  createChart() {
+    const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  
+    // Generisanje random vrednosti za achieved i tracked
+    const achievedData = Array.from({ length: 7 }, () => Math.floor(Math.random() * 100));
+    const trackedData = Array.from({ length: 7 }, () => Math.floor(Math.random() * 100));
+  
+    this.chart = new Chart("MyChart", {
+      type: 'line',
+      data: {
+        labels: daysOfWeek,
+        datasets: [
+          { label: "Achieved", data: achievedData },
+          { label: "Tracked", data: trackedData }
+        ]
+      },
+      options: {
+        aspectRatio: 2.5
+      }
+    });
+  }
+}
