6898846a2a9087f30f5f2270694f36ebb89e629f
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-03-17 19:14:45 +0100

    #39 Prebacena logika.

---
+++ server/Services/EmailService.cs
@@ -0,0 +1,33 @@
+﻿
+
+using MailKit.Security;
+using MimeKit.Text;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace server.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _configuration;
+        public EmailService(IConfiguration configuration)
+        { 
+            _configuration = configuration;
+        }   
+        public void SendEmail(EmailDto request)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(_configuration.GetSection("EmailSettings:From").Value));
+            email.To.Add(MailboxAddress.Parse(request.To));
+            email.Subject = request.Subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = request.Body };
+
+
+            using var smtp = new SmtpClient();
+            smtp.Connect(_configuration.GetSection("EmailSettings:SmtpServer").Value, 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_configuration.GetSection("EmailSettings:Username").Value, _configuration.GetSection("EmailSettings:Password").Value);
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
