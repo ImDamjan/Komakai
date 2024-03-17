5141ef7940d8a5ce5d51baf4da79da79f6ec3805
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-03-17 17:50:11 +0100

    #39 Dodato slanje maila pomocu laznog SMTP servisa. (Ethreal Email).

--- server/Controllers/EmailController.cs
+++ server/Controllers/EmailController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
+
 
 namespace server.Controllers
 {
@@ -7,5 +12,25 @@ namespace server.Controllers
     [ApiController]
     public class EmailController : ControllerBase
     {
+        [HttpPost]
+        public IActionResult SendEmail(string body)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("evalyn.turcotte15@ethereal.email"));
+            email.To.Add(MailboxAddress.Parse("evalyn.turcotte15@ethereal.email"));
+            email.Subject = "Test Email";
+            email.Body = new TextPart(TextFormat.Html) { Text = body };
+
+
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate("evalyn.turcotte15@ethereal.email", "jHmxWjgV5Ex38qBRfR");
+            smtp.Send(email);
+            smtp.Disconnect(true);
+
+
+            return Ok();
+
+        }
     }
 }
