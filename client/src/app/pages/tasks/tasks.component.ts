476f283315923195a034b0d0e6b7674967a20e82
aleksa.kosovac <49-2021@pmf.kg.ac.rs>
2024-03-17 19:10:37 +0100

    #19 Postavljeno ponavljanje task- komponente n puta

--- client/src/app/pages/tasks/tasks.component.ts
+++ client/src/app/pages/tasks/tasks.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  
+  n: number = 1; // Set your desired number of repetitions here
+
+  constructor() { }
+
+  getRange(n: number): number[] {
+    return Array.from({length: n}, (_, i) => i + 1);
+  }
 }
