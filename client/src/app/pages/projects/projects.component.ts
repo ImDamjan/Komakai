39d581cc0381c78c81f174779e173e6f5f3caab1
Anastasija Ilicic <45-2021@pmf.kg.ac.rs>
2024-03-19 22:57:34 +0100

    #51 Povezan overlay sa dugmetom

--- client/src/app/pages/projects/projects.component.ts
+++ client/src/app/pages/projects/projects.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CreateProjectOverlayComponent } from '../../components/create-project-overlay/create-project-overlay.component';
+
 
 @Component({
   selector: 'app-projects',
@@ -7,4 +10,10 @@ import { Component } from '@angular/core';
 })
 export class ProjectsComponent {
 
+  constructor(private dialog: MatDialog) { }
+
+  openCreateOverlay(): void {
+    const dialogRef = this.dialog.open(CreateProjectOverlayComponent, {
+    });
+  }
 }
