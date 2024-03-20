23686b246d95b2488069695fb92fd0bb58c62a9b
aleksa.kosovac <49-2021@pmf.kg.ac.rs>
2024-03-20 13:37:26 +0100

    #41 Zamenjen interfejs CanActivate tipom CanActivateFn

--- client/src/app/services/guard.service.ts
+++ client/src/app/services/guard.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthenticationService } from './atentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard{
 
   constructor(private authService: AuthenticationService, private router: Router) {}
 
-  canActivate(): boolean {
+  canActivate: CanActivateFn = () =>{
+    return this.checkLogin();
+  }
+
+  checkLogin(): boolean {
     if (this.authService.isAuthenticated()) {
       return true;
     } else {
