04d3622af3149636945897b123e4a5f3c5fac992
aleksa.kosovac <49-2021@pmf.kg.ac.rs>
2024-03-20 13:53:12 +0100

    #41 popravljena redirekcija

--- client/src/app/services/guard.service.ts
+++ client/src/app/services/guard.service.ts
@@ -17,7 +17,7 @@ export class AuthGuard{
     if (this.authService.isAuthenticated()) {
       return true;
     } else {
-      this.router.navigate(['/login']);
+      this.router.navigate(['/auth']);
       return false;
     }
   }
