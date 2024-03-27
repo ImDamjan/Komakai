09130057be26ef81a69823795cdd1bff1436e357
aleksa.kosovac <49-2021@pmf.kg.ac.rs>
2024-03-28 00:36:30 +0100

    #60 Odradjena logika za antigard, trenutno ne radi zbog problema sa ngoninit-om u app komponenti

--- client/src/app/services/guard.service.ts
+++ client/src/app/services/guard.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthenticationService } from './atentication.service';
 
@@ -6,6 +6,8 @@ import { AuthenticationService } from './atentication.service';
   providedIn: 'root'
 })
 export class AuthGuard{
+
+  isRedirected = false;
 
   constructor(private authService: AuthenticationService, private router: Router) {}
 
@@ -15,14 +17,22 @@ export class AuthGuard{
 
   checkLogin(): boolean {
     if (this.authService.isAuthenticated()) {
-      if (this.router.url.includes('/auth')) {
+      console.log("Logovao sam se")
+      console.log(this.router.url)
+      if (!this.isRedirected && this.router.url.includes('/auth')) {
+        console.log("Pokusavam da udjem na login, vraca me na dashboard")
         this.router.navigate(['/dashboard']);
-        return false;
+        this.isRedirected = true;
       }
       return true;
     } else {
+      console.log("Vraca me nazad na login")
       this.router.navigate(['/auth']);
       return false;
     }
+  }
+
+  ngOnDestroy() {
+    this.isRedirected = false;
   }
 }
