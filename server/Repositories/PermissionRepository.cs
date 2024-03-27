aa1664b27122c6242277d38f7d7581343ce5343e
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-03-27 23:46:59 +0100

    #58 Dodat repozitorijum za permisije.

---
+++ server/Repositories/PermissionRepository.cs
@@ -0,0 +1,14 @@
+﻿using server.Data;
+
+namespace server.Repositories
+{
+    public class PermissionRepository : IPermissionRepository
+    {
+        private readonly ProjectManagmentDbContext _context;
+
+        public PermissionRepository(ProjectManagmentDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
