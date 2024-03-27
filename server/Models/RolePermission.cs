f576880c802bd085e730cef870cc58e9be5284c0
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-03-27 23:26:50 +0100

    #58 Dodata nova tabela u bazu.

---
+++ server/Models/RolePermission.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace server.Models
+{
+    [Table("role_permissions")]
+    public class RolePermission
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [ForeignKey("RoleId")]
+        public virtual Role Role { get; set; }
+
+        [ForeignKey("PermissionId")]
+        public virtual Permission Permission { get; set; }
+    }
+}
