77b56b1c95c502dadd2c11d8830cdfb829eb50d3
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-03-28 15:27:24 +0100

    #58 Dodat mapper za permisije

---
+++ server/Mappers/PermissionMapper.cs
@@ -0,0 +1,17 @@
+﻿using server.DTOs.Permissions;
+using server.Models;
+
+namespace server.Mappers
+{
+    public static class PermissionMapper
+    {
+        public static PermissionDto ToPermissionDto(this Permission permission)
+        {
+            return new PermissionDto
+            {
+                Id = permission.Id,
+                Name = permission.Name
+            };
+        }
+    }
+}
