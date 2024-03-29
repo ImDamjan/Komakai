170ef67361fa2d5e831c070aa334e1c9da79c511
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-03-29 15:54:22 +0100

    #58 Izmenjen kontroler.

--- server/Controllers/RoleController.cs
+++ server/Controllers/RoleController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using server.DTOs.Permissions;
 using server.Interfaces;
 using server.Mappers;
+using server.Models;
 
 namespace server.Controllers
 {
@@ -13,9 +15,11 @@ namespace server.Controllers
     public class RoleController : ControllerBase
     {
         private readonly IRoleRepository _role_repo;
-        public RoleController(IRoleRepository role_repo)
+        private readonly IPermissionRepository _permission_repo;
+        public RoleController(IRoleRepository role_repo, IPermissionRepository permission_repo)
         {
             _role_repo = role_repo;
+            _permission_repo = permission_repo;
         }
         [HttpGet("getAll")]
         public async Task<IActionResult> GetAllRoles()
@@ -35,6 +39,38 @@ namespace server.Controllers
                 return NotFound("Role not found");
 
             return Ok(role.toRoleDto());
+        }
+
+        [HttpGet("{roleId}/permissions")]
+        public async Task<IActionResult> GetRolePermissions(int roleId)
+        {
+            var role = await _role_repo.GetRoleByIdAsync(roleId);
+            if (role == null)
+                return NotFound("Role not found");
+
+            var permissions = role.RolePermissions.Select(rp => rp.Permission).ToList();
+            var dtos = permissions.Select(p => p.ToPermissionDto()).ToList();
+            return Ok(dtos);
+        }
+
+        [HttpPost("{roleId}/assignPermission")]
+        public async Task<IActionResult> AssignPermissionToRole(int roleId, int permissionId)
+        {
+            var role = await _role_repo.GetRoleByIdAsync(roleId);
+            if (role == null)
+                return NotFound("Role not found");
+
+            var permission = await _permission_repo.GetPermissionByIdAsync(permissionId);
+            if (permission == null)
+                return NotFound("Permission not found");
+
+            if (role.RolePermissions.Any(rp => rp.PermissionId == permissionId))
+                return BadRequest("Permission is already assigned to the role");
+
+            role.RolePermissions.Add(new RolePermission { PermissionId = permissionId });
+            await _role_repo.SaveChangesAsync();
+
+            return Ok("Permission assigned to role successfully");
         }
     }
 }
