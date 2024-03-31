e460fc68a4a34f124eae666a3534482fe87fc59f
Anastasija Ilicic <45-2021@pmf.kg.ac.rs>
2024-03-31 23:42:52 +0200

    #65 Napravljeno getUsers

--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../enviroments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  baseUrl = environment.apiUrl;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getUsers(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/User`);
+  }
 }
