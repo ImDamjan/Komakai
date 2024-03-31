9b1ba791f1ee996ab7900d33c33f9af069b07b69
Anastasija Ilicic <45-2021@pmf.kg.ac.rs>
2024-03-31 23:49:50 +0200

    #65 Ubaceno da se vidi za team lead

--- client/src/app/components/create-project-overlay/create-project-overlay.component.ts
+++ client/src/app/components/create-project-overlay/create-project-overlay.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-create-project-overlay',
   templateUrl: './create-project-overlay.component.html',
   styleUrl: './create-project-overlay.component.css'
 })
-export class CreateProjectOverlayComponent {
+export class CreateProjectOverlayComponent implements OnInit {
+  users: any[] | undefined;
 
-  constructor(private dialogRef: MatDialogRef<CreateProjectOverlayComponent>) { }
+  constructor(private dialogRef: MatDialogRef<CreateProjectOverlayComponent>, private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe(users => {
+      this.users = users;
+    });
+  }
 
   closeOverlay(): void {
     // Close the overlay dialog
