0b4c69cb9784bfb0cb878370969dcfb1daf4ea3a
Anastasija Ilicic <45-2021@pmf.kg.ac.rs>
2024-04-01 02:19:55 +0200

    #65 Dodat model za project

---
+++ client/src/app/models/project.ts
@@ -0,0 +1,10 @@
+export interface Project {
+    userIds: string[];
+    priorityId: number;
+    title: string;
+    start: string;
+    end: string;
+    budget: number;
+    description: string;
+    type: string;
+}
