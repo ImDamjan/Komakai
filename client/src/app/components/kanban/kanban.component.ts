6d3bf9dd26859c062657f3b7f745144170a0e546
Stefan Vucicevic <stefanvucicevic002@gmail.com>
2024-04-02 01:17:10 +0200

    #69 uradjeno povezivanje sa stanjima

--- client/src/app/components/kanban/kanban.component.ts
+++ client/src/app/components/kanban/kanban.component.ts
@@ -4,6 +4,9 @@ import { Board } from '../../models/board.model';
 import { Column } from '../../models/column.model';
 import { TaskKanban } from '../../models/kanbantask';
 import { TaskCardKanbanComponent } from '../task-card-kanban/task-card-kanban.component';
+import { StateService } from '../../services/state.service';
+import { State } from '../../models/state';
+import { ProjectService } from '../../services/project.service';
 
 @Component({
   selector: 'app-kanban',
@@ -12,37 +15,27 @@ import { TaskCardKanbanComponent } from '../task-card-kanban/task-card-kanban.co
 })
 export class KanbanComponent implements OnInit{
   name = 'Angular Material ' + VERSION.major + ' Kanban board';
-  public board: Board = new Board('Kanban', [
-    new Column('Not started', '1', [
-      new TaskKanban('Some random idea', 'Opis zadatka 1', TaskCardKanbanComponent),
-      new TaskKanban('Neki tusk', 'Opis zadatka 2', TaskCardKanbanComponent)
-    ]),
-    new Column('Ready', '2', [
-      'Lorem ipsum',
-      'Task'
-    ]),
-    new Column('In progress', '3', [
-      'Lorem ipsum',
-      'foo'
-    ]),
-    new Column('Blocked', '4', [
-      'Lorem ipsum',
-      'foo'
-    ]),
-    new Column('Done', '5', [
-      'Lorem ipsum',
-      'foo'
-    ]),
-    new Column('Cancelled', '6', [
-      'Lorem ipsum',
-      'foo'
-    ])
-  ]);
+   public board: Board;
 
-  constructor(){}
+  constructor
+  (
+    private state_service : StateService
+  )
+  {
+    this.board = new Board('Kanban', []);
+  }
 
   public ngOnInit(): void {
-    console.log(this.board);
+    // console.log(this.board);
+    this.state_service.fetchAllStates().subscribe((states : State[])=>
+    {
+      let columns : Column[] = [];
+      states.forEach(state => {
+        columns.push(new Column(state.name,state.id + "",[]))
+      });
+      this.board.columns = columns;
+    },
+    error=> console.log(error));
   }
 
   public dropGrid(event: CdkDragDrop<TaskCardKanbanComponent[]>): void {
