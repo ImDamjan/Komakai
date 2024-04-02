f0e3ce9f7a0371b512be84dbf74bbae0377c2f3c
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-04-02 23:57:47 +0200

    #58 Dodata mapper klasa.

---
+++ server/Mappers/UserProjectRolesMapper.cs
@@ -0,0 +1,19 @@
+﻿using server.DTOs.Permissions;
+using server.Models;
+
+namespace server.Mappers
+{
+    public class UserProjectRolesMapper
+    {
+        public static UserProjectRolesDto MapToDto(UserProjectRoles upr)
+        {
+            return new UserProjectRolesDto
+            {
+                Id = upr.Id,
+                UserId = upr.UserId,
+                ProjectId = upr.ProjectId,
+                RoleId = upr.RoleId,
+            };
+        }
+    }
+}
