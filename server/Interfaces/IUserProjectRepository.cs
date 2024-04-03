cc9cad7e18aa9c654990d87d816c78cc3958d159
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-04-03 22:19:31 +0200

    #58 Dodate metode u interfejs.

--- server/Interfaces/IUserProjectRepository.cs
+++ server/Interfaces/IUserProjectRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace server.Interfaces
+﻿using server.Models;
+
+namespace server.Interfaces
 {
     public interface IUserProjectRepository
     {
+        Task<IEnumerable<UserProjectRoles>> GetAllUserProjectRoles();
+        Task<UserProjectRoles> GetUserProjectRolesById(int id);
+        Task<UserProjectRoles> AddUserProjectRoles(UserProjectRoles userProjectRoles);
     }
 }
