8775862f9edbb7812193c325dfcb6bc58bc1236f
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-04-03 22:23:39 +0200

    #58 Dodate metode u repozitorijum.

--- server/Repositories/UserProjectRolesRepository.cs
+++ server/Repositories/UserProjectRolesRepository.cs
@@ -1,6 +1,31 @@
-﻿namespace server.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using server.Data;
+using server.Models;
+
+namespace server.Repositories
 {
-    public class UserProjectRolesRepository
+    public class UserProjectRolesRepository : IUserProjectRepository
     {
+        private readonly ProjectManagmentDbContext _context;
+        public UserProjectRolesRepository(ProjectManagmentDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<UserProjectRoles> AddUserProjectRoles(UserProjectRoles userProjectRoles)
+        {
+            _context.UserProjectRoles.Add(userProjectRoles);
+            await _context.SaveChangesAsync();
+            return userProjectRoles;
+        }
+
+        public async Task<IEnumerable<UserProjectRoles>> GetAllUserProjectRoles()
+        {
+            return await _context.UserProjectRoles.ToListAsync();
+        }
+
+        public async Task<UserProjectRoles> GetUserProjectRolesById(int id)
+        {
+            return await _context.UserProjectRoles.FindAsync(id);
+        }
     }
 }
