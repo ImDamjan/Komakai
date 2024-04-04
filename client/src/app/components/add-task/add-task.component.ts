e54280b6ef63edcbb695035a07a1399ba4f92b7f
Aleksandra Stanic <astanic2001@gmail.com>
2024-04-04 17:00:29 +0200

    #70-Pocetak pravljenja add task komponenete

---
+++ client/src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { CreateProjectOverlayComponent } from '../create-project-overlay/create-project-overlay.component';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrl: './add-task.component.css'
+})
+export class AddTaskComponent {
+  constructor(private dialogRef: MatDialogRef<CreateProjectOverlayComponent>) { }
+
+  closeOverlay(): void {
+    // Close the overlay dialog
+    this.dialogRef.close();
+  }
+}
