f5fc3b43a1fde0235a95a60c99cfc7791a26d75d
Aleksandra Stanic <astanic2001@gmail.com>
2024-04-06 12:25:19 +0200

    #74-Priprema za gantogram

---
+++ client/src/app/components/gantogram/gantogram.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { SvelteGantt, SvelteGanttTable } from 'svelte-gantt';
+
+@Component({
+  selector: 'app-gantogram',
+  templateUrl: './gantogram.component.html',
+  styleUrl: './gantogram.component.css'
+})
+export class GantogramComponent {
+
+}
