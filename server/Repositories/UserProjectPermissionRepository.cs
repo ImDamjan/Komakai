53eedb49e3ee46958dea909343ea377ff059569c
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-04-08 23:49:00 +0200

    #58 kreiran repozitorijum

---
+++ server/Repositories/UserProjectPermissionRepository.cs
@@ -0,0 +1,14 @@
+﻿using server.Data;
+
+namespace server.Repositories
+{
+    public class UserProjectPermissionRepository
+    {
+        private readonly ProjectManagmentDbContext _context;
+
+        public UserProjectPermissionRepository(ProjectManagmentDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
