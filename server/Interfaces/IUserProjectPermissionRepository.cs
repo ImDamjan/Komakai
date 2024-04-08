60985417953abd3aa040d55b820bfcb6b7b1be13
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-04-08 23:47:39 +0200

    #58 kreiran interfejs

---
+++ server/Interfaces/IUserProjectPermissionRepository.cs
@@ -0,0 +1,11 @@
+﻿using server.Models;
+
+namespace server.Interfaces
+{
+    public interface IUserProjectPermissionRepository
+    {
+        Task AddUserProjectPermission(UserProjectPermission userProjectPermission);
+        Task RemoveUserProjectPermission(UserProjectPermission userProjectPermission);
+        Task<UserProjectPermission> GetUserProjectPermissionById(int id);
+    }
+}
