d89c60406a0ebe16a4088f54c2ce9d60598fd434
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-04-08 23:54:41 +0200

    #58 Nasledjen interfejs.

--- server/Repositories/UserProjectPermissionRepository.cs
+++ server/Repositories/UserProjectPermissionRepository.cs
@@ -1,14 +1,20 @@
 ﻿using server.Data;
+using server.Models;
 
 namespace server.Repositories
 {
-    public class UserProjectPermissionRepository
+    public class UserProjectPermissionRepository : IUserProjectPermissionRepository
     {
         private readonly ProjectManagmentDbContext _context;
 
         public UserProjectPermissionRepository(ProjectManagmentDbContext context)
         {
             _context = context;
+        }
+        public async Task AddUserProjectPermission(UserProjectPermission userProjectPermission)
+        {
+            _context.UserProjectPermissions.Add(userProjectPermission);
+            await SaveChangesAsync();
         }
     }
 }
