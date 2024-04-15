4b16b3c82c00421fbbea6d7704c5aded59d05471
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-04-15 22:54:31 +0200

    #58 Dodata klasa

---
+++ server/Authorization/HasPermissionFilter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace server.Authorization
+{
+    public class HasPermissionFilter : IAsyncAuthorizationFilter
+    {
+        private readonly Permisija _permission;
+        private readonly IAuthorizationService _authorizationService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public HasPermissionFilter(Permisija permission, IAuthorizationService authorizationService, IHttpContextAccessor httpContextAccessor)
+        {
+            _permission = permission;
+            _authorizationService = authorizationService;
+            _httpContextAccessor = httpContextAccessor;
+        }
+    }
+}
