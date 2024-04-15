29b43b57388a210b9a0b70d72da39e423c8f44c1
aleksa.kosovac <49-2021@pmf.kg.ac.rs>
2024-04-15 22:56:32 +0200

    #76 Ubaceni selekti za state i priority

--- client/src/app/components/filter-details/filter-details.component.ts
+++ client/src/app/components/filter-details/filter-details.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { State } from '../../models/state/state';
+import { Priority } from '../../models/priority/priority';
+import { StateService } from '../../services/state.service';
+import { PriorityService } from '../../services/priority.service';
 
 @Component({
   selector: 'app-filter-details',
@@ -6,5 +10,25 @@ import { Component } from '@angular/core';
   styleUrl: './filter-details.component.css'
 })
 export class FilterDetailsComponent {
+
+  states: State[] = [];
+
+  priorities: Priority[] = [];
+
+  constructor(private state: StateService,private priority: PriorityService){}
+
+  ngOnInit(){
+
+    this.state.fetchAllStates().subscribe(states =>{
+      this.states = states;
+      // console.log(states);
+    })
+
+    this.priority.getPriorities().subscribe(priorities =>{
+      this.priorities = priorities;
+      console.log(this.priorities);
+    })
+
+  }
 
 }
