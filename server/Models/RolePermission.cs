e226ab0622a26fa53ccb62a441954cad448840b1
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-04-16 20:06:10 +0200

    #58 Dodat model

---
+++ server/Models/RolePermission.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Models
+{
+    public class RolePermission
+    {
+        [Key] 
+        public int Id { get; set; }
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+
+        public int PermissionId { get; set; }
+        public Permission Permission { get; set; }
+    }
+}
