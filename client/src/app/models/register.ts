fe6d38cf5ef189149ea1afb0bf328e7c83966dff
Stefan Vucicevic <stefanvucicevic002@gmail.com>
2024-04-17 18:55:39 +0200

    #93 ubacen servis za role i napravljen je register interfejs

---
+++ client/src/app/models/register.ts
@@ -0,0 +1,9 @@
+export interface Register {
+    username: string,
+    name: string,
+    lastname: string,
+    password: string,
+    email: string,
+    confirmPassword: string,
+    roleId: number
+}
