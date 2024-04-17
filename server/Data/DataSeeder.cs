53150e1bfdfc1da0e91438a0fbbd3ff46987a6d8
Stefan Vucicevic <stefanvucicevic002@gmail.com>
2024-04-18 00:11:49 +0200

    isprvaljena sitna greska

--- server/Data/DataSeeder.cs
+++ server/Data/DataSeeder.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using server.Authorization;
-using server.Migrations;
 using server.Models;
 
 namespace server.Data
