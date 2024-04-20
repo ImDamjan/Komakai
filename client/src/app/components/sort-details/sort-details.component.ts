3c625b475efd35790a9b3972c11101b4584e1f6a
aleksa.kosovac <49-2021@pmf.kg.ac.rs>
2024-04-20 17:16:04 +0200

    #76 Login pritiskom na enter radi

--- client/src/app/components/sort-details/sort-details.component.ts
+++ client/src/app/components/sort-details/sort-details.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { State } from '../../models/state/state';
+import { Priority } from '../../models/priority/priority';
+import { StateService } from '../../services/state.service';
+import { PriorityService } from '../../services/priority.service';
+import { MAT_DIALOG_DATA,MatDialogRef } from '@angular/material/dialog';
+import moment from 'moment';
+import { Form, FormBuilder, FormGroup } from '@angular/forms';
+import { TaskFilter } from '../../models/task/task-filter';
+import { FilterDataService } from '../../services/filterdata.service';
+import { ProjectService } from '../../services/project.service';
+import { Project } from '../../models/project/project';
 
 @Component({
   selector: 'app-sort-details',
@@ -6,5 +17,24 @@ import { Component } from '@angular/core';
   styleUrl: './sort-details.component.css'
 })
 export class SortDetailsComponent {
+
+  private data : any =  inject(MAT_DIALOG_DATA)
+
+  filter: TaskFilter = {};
+
+  sortGroup: FormGroup;
+
+  private sortDialog = inject(MatDialogRef<SortDetailsComponent>);
+
+  constructor(private fb: FormBuilder){
+    this.sortGroup = this.fb.group({
+      property: [''],
+      flag: [null]
+    })
+  }
+
+  ngOnInit(){
+    this.filter = this.data[0];
+  }
 
 }
