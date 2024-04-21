f4fe5ee65a69001ffaa7957d03894b84450e6f6e
Anastasija Ilicic <45-2021@pmf.kg.ac.rs>
2024-04-21 23:49:49 +0200

    #91 Edit profile radi

--- client/src/app/components/profile-details/profile-details.component.ts
+++ client/src/app/components/profile-details/profile-details.component.ts
@@ -1,7 +1,10 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { RoleService } from '../../services/role.service';
 import { Role } from '../../models/role';
+import { UserService } from '../../services/user.service';
+import { UpdateUser } from '../../models/user/update-user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile-details',
@@ -9,14 +12,16 @@ import { Role } from '../../models/role';
   styleUrl: './profile-details.component.css'
 })
 export class ProfileDetailsComponent {
-  user: any;
+  user: UpdateUser;
   roleId!: number;
   roles!: Role[];
   editMode = false;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private roleService: RoleService) {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private roleService: RoleService, private userService: UserService, private router: Router) {
     this.user = data.user;
     this.roleId = data.role;
+    this.user.roleId = this.roleId;
+    console.log(this.user);
   }
 
   ngOnInit() : void {
@@ -27,5 +32,11 @@ export class ProfileDetailsComponent {
 
   toggleEditMode() {
     this.editMode = !this.editMode;
+  }
+
+  editProfile(userid: Number): void {
+    this.userService.updateUser(this.user).subscribe(response => {
+      alert('Project edited successfully!');
+    });  
   }
 }
