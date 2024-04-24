d67bd883c29feef8545a61e8ce0a06d4652df9ea
Stefan Vucicevic <stefanvucicevic002@gmail.com>
2024-04-24 10:40:52 +0200

    #101 sredjen css za prioritet

--- client/src/app/components/task-card-kanban/task-card-kanban.component.ts
+++ client/src/app/components/task-card-kanban/task-card-kanban.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, Input } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
 import { Task } from '../../models/task/task';
 
 @Component({
@@ -6,6 +6,39 @@ import { Task } from '../../models/task/task';
   templateUrl: './task-card-kanban.component.html',
   styleUrl: './task-card-kanban.component.css'
 })
-export class TaskCardKanbanComponent {
-    @Input() public task! : Task;
+export class TaskCardKanbanComponent implements OnInit {
+  @Input() public task! : Task;
+  ngOnInit(): void {
+    this.task.end = new Date(this.task.end);
+    this.task.start = new Date(this.task.start);
+  }
+
+  public getPriorityClass()
+  {
+    if(this.task.priority.description.toLowerCase() === "low")
+      return "low";
+    if(this.task.priority.description.toLowerCase() === "at risk")
+      return "at-risk";
+    if(this.task.priority.description.toLowerCase() === "medium")
+      return "medium";
+    if(this.task.priority.description.toLowerCase() === "high")
+      return "high";
+    
+    return "";
+
+
+  }
+
+  public transformDate(date : Date) : string
+  {
+    let splited =date.toLocaleDateString().split('/');
+    for (let i = 0; i < splited.length; i++) {
+      if(splited[i].length == 1)
+        {
+          splited[i] = "0"+splited[i];
+        }
+      
+    }
+    return splited[2] + "-" + splited[0] + "-" + splited[1];
+  }
 }
