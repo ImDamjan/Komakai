b3a453fd5056f753cdd130e7e25b74504fe27536
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-04-25 23:01:31 +0200

    #99 Kreiran repozitorijum sa interfejsom.

---
+++ server/Repositories/AnswerRepository.cs
@@ -0,0 +1,41 @@
+﻿using server.Data;
+using server.DTOs.Answer;
+using server.Models;
+
+namespace server.Repositories
+{
+    public class AnswerRepository:IAnswerRepository
+    {
+        private readonly ProjectManagmentDbContext _context;
+
+        public AnswerRepository(ProjectManagmentDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Answer> CreateAnswerAsync(Answer answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Answer?> DeleteAnswerByIdAsync(int answerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Answer>> GetAllAnswersByCommentIdAsync(int commentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Answer?> GetAnswerByIdAsync(int answerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Answer> UpdateAnswerAsync(UpdateAnswerDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
