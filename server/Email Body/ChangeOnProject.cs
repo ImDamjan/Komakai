07e793f51e946a680f20b1029c82fd4ab6ab4e67
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-05-02 23:50:45 +0200

    #104 Dodat email body.

--- server/Email Body/ChangeOnProject.cs
+++ server/Email Body/ChangeOnProject.cs
@@ -2,5 +2,16 @@
 {
     public class ChangeOnProject
     {
+        public static string GetHtmlBody(string projectName)
+        {
+            return $@"
+                <html>
+                <body>
+                    <p>Dear Worker,</p>
+                    <p>This is to inform you that there has been a change in the project '{projectName}'.</p>
+                    <p>Regards,<br/>Your Project Team</p>
+                </body>
+                </html>";
+        }
     }
 }
