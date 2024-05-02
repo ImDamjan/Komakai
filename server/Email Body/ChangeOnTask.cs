789a874b59914551aad9a18d24703233dd536f95
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-05-02 23:50:31 +0200

    #104 Dodat email body.

--- server/Email Body/ChangeOnTask.cs
+++ server/Email Body/ChangeOnTask.cs
@@ -2,5 +2,16 @@
 {
     public class ChangeOnTask
     {
+        public static string GetHtmlBody(string taskName)
+        {
+            return $@"
+                <html>
+                <body>
+                    <p>Dear Worker,</p>
+                    <p>This is to inform you that there has been a change in the task '{taskName}'.</p>
+                    <p>Regards,<br/>Your Project Team</p>
+                </body>
+                </html>";
+        }
     }
 }
