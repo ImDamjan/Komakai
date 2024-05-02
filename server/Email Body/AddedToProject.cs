89cdafc07bbda9cf773451c4cac59f1a5f543a7c
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-05-02 23:55:48 +0200

    #104 Dodat email body.

--- server/Email Body/AddedToProject.cs
+++ server/Email Body/AddedToProject.cs
@@ -2,5 +2,16 @@
 {
     public class AddedToProject
     {
+        public static string GetHtmlBody(string projectName)
+        {
+            return $@"
+                <html>
+                <body>
+                    <p>Dear Worker,</p>
+                    <p>You've been added to the project '{projectName}'.</p>
+                    <p>Regards,<br/>Your Project Team</p>
+                </body>
+                </html>";
+        }
     }
 }
