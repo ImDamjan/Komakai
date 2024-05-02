4897f747b9d704174e1c72865dfdd8ae24742894
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-05-02 23:50:03 +0200

    #104 Dodat Dto

---
+++ server/DTOs/Email/NotificationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace server.DTOs.Email
+{
+    public class NotificationDto
+    {
+        public string To { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string NotificationType { get; set; } = string.Empty;
+        public string TaskOrProjectName { get; set; } = string.Empty;
+    }
+}
