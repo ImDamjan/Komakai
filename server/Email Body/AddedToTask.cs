dd88e34668853072931fa306a1349e337a4028eb
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-05-02 23:53:15 +0200

    #104 Dodat email body.

--- server/Email Body/AddedToTask.cs
+++ server/Email Body/AddedToTask.cs
@@ -2,5 +2,16 @@
 {
     public class AddedToTask
     {
+        public static string GetHtmlBody(string taskName)
+        {
+            return $@"
+                <html>
+                <body>
+                    <p>Dear Worker,</p>
+                    <p>You've been added to the task '{taskName}'.</p>
+                    <p>Regards,<br/>Your Project Team</p>
+                </body>
+                </html>";
+        }
     }
 }
