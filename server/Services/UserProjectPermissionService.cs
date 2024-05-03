9aaff49dbfab6a1c7b6f1b40c316e1ed26e24d70
Damjan Pavlovic <damjan.pavlovic11@gmail.com>
2024-05-04 00:11:17 +0200

    #104 Kreiran servis

---
+++ server/Services/UserProjectPermissionService.cs
@@ -0,0 +1,29 @@
+﻿using server.DTOs.Permissions;
+using server.Models;
+
+namespace server.Services
+{
+    public class UserProjectPermissionService:IUserProjectPermissionService
+    {
+        private readonly IUserProjectPermissionRepository _userProjectPermissionRepository;
+
+        public UserProjectPermissionService(IUserProjectPermissionRepository userProjectPermissionRepository)
+        {
+            _userProjectPermissionRepository = userProjectPermissionRepository;
+        }
+
+        public async Task AddUserProjectPermissions(UserProjectPermissionArrayDto userProjectPermissionArrayDto)
+        {
+            // Extract individual UserProjectPermissionDto objects
+            var userProjectPermissions = userProjectPermissionArrayDto.Permissions.Select(dto => new UserProjectPermission
+            {
+                UserId = dto.UserId, // Assuming you have the user ID
+                ProjectId = dto.ProjectId, // Assuming you have the project ID
+                PermissionId = dto.PermissionId
+            });
+
+            // Add permissions to the repository (bulk insertion)
+            await _userProjectPermissionRepository.AddUserProjectPermissions(userProjectPermissions);
+        }
+    }
+}
