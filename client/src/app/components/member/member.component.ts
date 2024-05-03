0933f0a0063bacd7b5c5e3edc480657e77805637
Anastasija Ilicic <45-2021@pmf.kg.ac.rs>
2024-05-04 00:15:16 +0200

    #111 Povezana slika za member

--- client/src/app/components/member/member.component.ts
+++ client/src/app/components/member/member.component.ts
@@ -5,20 +5,29 @@ import { UpdateUser } from '../../models/user/update-user';
 import { UserService } from '../../services/user.service';
 import { error } from 'console';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { EMPTY, catchError, forkJoin, tap } from 'rxjs';
 
 @Component({
   selector: 'app-member',
   templateUrl: './member.component.html',
   styleUrl: './member.component.css'
 })
-export class MemberComponent {
+export class MemberComponent implements OnInit{
 
   @Input() roles :Role[] = [];
   @Input() users : User[] = [];
+  allUsers: User[] = [];
   private user_service = inject(UserService);
   private spinner = inject(NgxSpinnerService);
+  profilePicturesLoaded = false;
 
-
+  ngOnInit(): void {
+    this.user_service.getUsers().subscribe(users => {
+      this.allUsers = users;
+      console.log(this.allUsers)
+      this.profilePicture(this.allUsers);
+    });
+  }
 
   updateUserRole(role: Role,user : User)
   {
@@ -56,6 +65,26 @@ export class MemberComponent {
     this.user_service.updateUser(body).subscribe({
       next: (updatedUser: User) => {user = updatedUser; confirm("User updated successfully");this.spinner.hide();},
       error: (err) => console.log(err)
+    });
+  }
+
+  profilePicture(users: User[]) {
+    this.users.forEach(user => {
+      this.user_service.profilePicture(user.id).subscribe({
+        next: (message: { profilePicture: string, type: string }) => {
+          user.profile_picture = `data:${message.type};base64,${message.profilePicture}`;
+          console.log(user.profile_picture);
+        }, 
+        error: (err) => {
+          if (err.error === 'Profile picture not found for the user') {
+            user.profile_picture = "../../../assets/pictures/defaultpfp.svg";
+            console.log(user.profile_picture);
+          }
+        },
+        complete: () => {
+          this.profilePicturesLoaded = true;
+        }
+      });
     });
   }
 }
