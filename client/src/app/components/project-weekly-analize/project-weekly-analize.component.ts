db44089a9bc507c50bc7e05cdb6fa26bf1ae89a5
Anastasija Ilicic <45-2021@pmf.kg.ac.rs>
2024-05-22 17:40:29 +0200

    #141 Popravljen izgled grafika

--- client/src/app/components/project-weekly-analize/project-weekly-analize.component.ts
+++ client/src/app/components/project-weekly-analize/project-weekly-analize.component.ts
@@ -13,8 +13,8 @@ import { Task } from '../../models/task/task';
 })
 export class ProjectWeeklyAnalizeComponent implements OnInit{
 
-  @Input()projects!: Project[];
-  @Input()tasks!: Task[];
+  @Input() projects!: Project[];
+  @Input() tasks!: Task[];
   atRiskCount = 0;
   highCount = 0;
   mediumCount = 0;
@@ -26,90 +26,112 @@ export class ProjectWeeklyAnalizeComponent implements OnInit{
   plowCount = 0;
 
   constructor() { }
+
   ngOnInit(): void {
     this.createChart();
   }
+
   public chart: any;
 
   createChart() {
-          
-          this.tasks.forEach(task => {
-            switch (task.priority.id) {
-              case 1:
-                this.atRiskCount++;
-                break;
-              case 2:
-                this.highCount++;
-                break;
-              case 3:
-                this.mediumCount++;
-                break;
-              case 4:
-                this.lowCount++;
-                break;
-              default:
-                break;
-            }
-          });
+    this.tasks.forEach(task => {
+      switch (task.priority.id) {
+        case 1:
+          this.atRiskCount++;
+          break;
+        case 2:
+          this.highCount++;
+          break;
+        case 3:
+          this.mediumCount++;
+          break;
+        case 4:
+          this.lowCount++;
+          break;
+        default:
+          break;
+      }
+    });
 
-          this.projects.forEach(project => {
-            switch (project.priority.id) {
-              case 1:
-                this.patRiskCount++;
-                break;
-              case 2:
-                this.phighCount++;
-                break;
-              case 3:
-                this.pmediumCount++;
-                break;
-              case 4:
-                this.plowCount++;
-                break;
-              default:
-                break;
-            }
-          });
+    this.projects.forEach(project => {
+      switch (project.priority.id) {
+        case 1:
+          this.patRiskCount++;
+          break;
+        case 2:
+          this.phighCount++;
+          break;
+        case 3:
+          this.pmediumCount++;
+          break;
+        case 4:
+          this.plowCount++;
+          break;
+        default:
+          break;
+      }
+    });
 
-          const daysOfWeek = ['At risk', 'High', 'Medium', 'Low'];
-    
-        this.chart = new Chart("MyChart", {
-          type: 'bar',
-          data: {
-            labels: daysOfWeek,
-            datasets: [
-              { label: "Projects", data: [this.patRiskCount,this.phighCount,this.pmediumCount,this.plowCount], backgroundColor: 'rgba(0, 128, 128)' },
-              { label: "Tasks", data: [this.atRiskCount,this.highCount,this.mediumCount,this.lowCount], backgroundColor: 'rgba(255, 127, 80)' }
-            ]
+    const priorityLabels = ['At Risk', 'High', 'Medium', 'Low'];
+
+    this.chart = new Chart("MyChart", {
+      type: 'bar',
+      data: {
+        labels: priorityLabels,
+        datasets: [
+          {
+            label: "Projects",
+            data: [this.patRiskCount, this.phighCount, this.pmediumCount, this.plowCount],
+            backgroundColor: 'rgba(0, 128, 128, 0.8)',
+            borderColor: 'rgba(0, 128, 128, 0.2)',
+            borderWidth: 2
           },
-          options: {
-            aspectRatio: 2.5,
-            responsive: true,
-            maintainAspectRatio: false,
-            plugins: {
-              legend: {
-                onClick: function() {} 
-              },
-              title: {
-                display: true,
-                text: 'Priority Overview',
-                font: {
-                  size: 18
-                }
-              }
-            },
-            indexAxis: 'y',
-            scales: {
-              x: {
-                stacked: false
-              },
-              y: {
-                stacked: false
-              }
+          {
+            label: "Tasks",
+            data: [this.atRiskCount, this.highCount, this.mediumCount, this.lowCount],
+            backgroundColor: 'rgba(255, 127, 80, 0.8)',
+            borderColor: 'rgba(255, 127, 80, 0.2)',
+            borderWidth: 2
+          }
+        ]
+      },
+      options: {
+        aspectRatio: 2.5,
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            onClick: function() {} 
+          },
+          title: {
+            display: true,
+            text: 'Priority Overview',
+            font: {
+              size: 18
             }
           }
-        });
-
-    
+        },
+        indexAxis: 'y',
+        scales: {
+          x: {
+            title: {
+              display: true,
+              text: 'Quantity'
+            },
+            beginAtZero: true,
+            ticks: {
+              stepSize: 1
+            }
+          },
+          y: {
+            title: {
+              display: true,
+              text: 'Priority name'
+            },
+            beginAtZero: true,
+          }
+        }
+      }
+    });
   }
 }
--- client/src/app/components/task-tags/task-tags.component.ts
+++ client/src/app/components/task-tags/task-tags.component.ts
@@ -11,72 +11,87 @@ import { Project } from '../../models/project/project';
 export class TaskTagsComponent implements AfterViewInit{
   @ViewChild('myChart') myChart!: ElementRef;
 
-  @Input()projects!: Project[];
-  
+  @Input() projects!: Project[];
+
   ngAfterViewInit() {
     this.createChart();
   }
 
   createChart() {
-      const projectNames: string[] = [];
-      const completionPercentages: number[] = [];
-      this.projects.forEach(project => {
-          projectNames.push(project.title);
-          completionPercentages.push(project.percentage);
-      });
-      
+    const projectNames: string[] = [];
+    const completionPercentages: number[] = [];
+    this.projects.forEach(project => {
+      projectNames.push(project.title);
+      completionPercentages.push(project.percentage);
+    });
 
-      const incompleteProjects = [];
-      for (let i = 0; i < projectNames.length; i++) {
-        if (completionPercentages[i] < 100) {
-          incompleteProjects.push({ name: projectNames[i], percentage: completionPercentages[i] });
-        }
+    const incompleteProjects = [];
+    for (let i = 0; i < projectNames.length; i++) {
+      if (completionPercentages[i] < 100) {
+        incompleteProjects.push({ name: projectNames[i], percentage: completionPercentages[i] });
       }
+    }
 
-      incompleteProjects.sort((a, b) => b.percentage - a.percentage);
-      const topIncompleteProjects = incompleteProjects.slice(0, 5);
+    incompleteProjects.sort((a, b) => b.percentage - a.percentage);
+    const topIncompleteProjects = incompleteProjects.slice(0, 5);
 
-      const ctx = this.myChart.nativeElement.getContext('2d');
-      new Chart(ctx, {
-        type: 'bar',
-        data: {
-          labels: topIncompleteProjects.map(project => project.name),
-          datasets: [{
-            data: topIncompleteProjects.map(project => project.percentage),
-            backgroundColor: 'rgb(116, 226, 145, 0.8)',
-            borderColor: 'rgb(116, 226, 145, 0.2)',
-            borderWidth: 2
-          }]
-        },
-        options: {
-          indexAxis: 'y',
-          scales: {
-            x: {
-              beginAtZero: true,
-              max: 100
-            },
-            y: {
-              beginAtZero: true,
-              max: 100
-            }
-          },
-          responsive: true,
-          maintainAspectRatio: false,
-          plugins: {
+    const ctx = this.myChart.nativeElement.getContext('2d');
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: topIncompleteProjects.map(project => project.name),
+        datasets: [{
+          data: topIncompleteProjects.map(project => project.percentage),
+          backgroundColor: 'rgba(116, 226, 145, 0.8)',
+          borderColor: 'rgba(116, 226, 145, 0.2)',
+          borderWidth: 2
+        }]
+      },
+      options: {
+        indexAxis: 'y',
+        scales: {
+          x: {
+            beginAtZero: true,
+            max: 100,
             title: {
               display: true,
-              text: 'Top 5 Incomplete Projects by Completion Percentage',
-              font: {
-                size: 18
-              }
+              text: 'Completion Percentage'
             },
-            legend: {
-              display: false
+            ticks: {
+              callback: function(value) {
+                return value + '%';
+              }
+            }
+          },
+          y: {
+            title: {
+              display: true,
+              text: 'Project Names'
+            }
+          }
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          title: {
+            display: true,
+            text: 'Top 5 Incomplete Projects by Completion Percentage',
+            font: {
+              size: 18
+            }
+          },
+          legend: {
+            display: false
+          },
+          tooltip: {
+            callbacks: {
+              label: function(context) {
+                return context.raw + '%';
+              }
             }
           }
         }
-      });
-
-    
+      }
+    });
   }
 }
