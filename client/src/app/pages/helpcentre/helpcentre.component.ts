176f77866c489397a5441713f9bb35f8dcbf39c6
aleksa.kosovac <49-2021@pmf.kg.ac.rs>
2024-06-02 23:36:27 +0200

    #178 UBACEN PRIKAZ PDF-A ZA PROJECT MANAGER-A HVALA ISUSU

--- client/src/app/pages/helpcentre/helpcentre.component.ts
+++ client/src/app/pages/helpcentre/helpcentre.component.ts
@@ -1,5 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
 import { Component, inject } from '@angular/core';
 import { JwtDecoderService } from '../../services/jwt-decoder.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+// @Pipe({
+//   name: 'safe'
+// })
 
 @Component({
   selector: 'app-helpcentre',
@@ -12,12 +18,31 @@ export class HelpcentreComponent {
 
   public role : string = "";
 
+  public pdfUrls: { [role: string]: string } = {
+    // Guest: 'assets/help-documents/guest-guide.pdf',
+    // Developer: 'assets/help-documents/developer-guide.pdf',
+    ProjectManager: 'assets/help-documents/project-manager-guide.pdf',
+    Admin: 'assets/help-documents/admin-guide.pdf'
+  };
+
+  constructor(private sanitizer: DomSanitizer) {}
+
   ngOnInit(): void {
     let token = this.jwtService.getToken();
     if(token!=null)
     {
       let decode = this.jwtService.decodeToken(token);
       this.role = decode.role;
+      if(this.role = "Project Manager"){
+        this.role = "ProjectManager";
+      }
     }
+  }
+
+  public getPdfUrl(role: string){
+    if (this.pdfUrls[role]) {
+      return this.sanitizer.bypassSecurityTrustResourceUrl(this.pdfUrls[role]);
+    }
+    return null;
   }
 }
